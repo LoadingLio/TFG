38ddada3ac9dad5b744646ab34255e5fb6f57f28
LoadingLio <151134188+LoadingLio@users.noreply.github.com>
2025-01-29 16:43:15 +0100

    Refactor login functionality and enhance UI styles

--- LogScreen/login.php
+++ LogScreen/login.php
@@ -6,7 +6,7 @@ session_start();
 $servername = "database-1.cba00ygu8qru.eu-north-1.rds.amazonaws.com";
 $username = "admin";
 $password = "Raul24h3rm4n0!";
-$dbname = "TFG"; // Asegúrate de que este sea el nombre correcto de la base de datos
+$dbname = "TFG";
 
 // Conectar a la base de datos con PDO
 try {
@@ -19,42 +19,32 @@ try {
 
 // Verificar si se ha enviado el formulario
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Validar los datos
-    if (isset($_POST["email"]) && isset($_POST["password"])) {
-        $email = trim($_POST["email"]);
-        $pass = trim($_POST["password"]);
+    // Extraer valores del formulario
+    $correo = trim($_POST['correo'] ?? '');
+    $contraseña = trim($_POST['contraseña'] ?? '');
 
-        // Verificar que los campos no estén vacíos
-        if (empty($email) || empty($pass)) {
-            echo "Por favor, complete todos los campos.";
-            exit;
-        }
-
-        // Preparar la consulta SQL
-        $stmt = $conn->prepare("SELECT * FROM Usuarios WHERE correo = :email AND contraseña = :password");
-        $stmt->bindParam(":email", $email);
-        $stmt->bindParam(":password", $pass);
-
-        // Ejecutar la consulta SQL
-        try {
-            $stmt->execute();
-            $result = $stmt->fetch(PDO::FETCH_ASSOC);
-
-            if ($result) {
-                // Iniciar la sesión
-                $_SESSION['email'] = $email;
-                header("Location: login_exitoso.html");
-                exit;
-            } else {
-                echo "Credenciales incorrectas.";
-                exit;
-            }
-        } catch (PDOException $e) {
-            echo "Error de consulta: " . $e->getMessage();
-            exit;
-        }
-    } else {
+    // Verificar que los campos no estén vacíos
+    if (empty($correo) || empty($contraseña)) {
         echo "Por favor, complete todos los campos.";
+        exit;
+    }
+
+    // Encriptar la contraseña
+    $contraseña_encriptada = password_hash($contraseña, PASSWORD_DEFAULT);
+
+    // Preparar la consulta SQL
+    $stmt = $conn->prepare("SELECT * FROM Usuarios WHERE correo = ?");
+    $stmt->execute(array($correo));
+    $row = $stmt->fetch();
+
+    // Verificar si la contraseña es correcta
+    if (password_verify($contraseña, $row['contraseña'])) {
+        // Iniciar la sesión
+        $_SESSION['correo'] = $correo;
+        header("Location: login_exitoso.html");
+        exit;
+    } else {
+        echo "Credenciales incorrectas.";
         exit;
     }
 }
