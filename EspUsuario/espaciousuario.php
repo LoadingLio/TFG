97e4c40a6e86a17e697cc70994d9fbdda540f48f
Raúl Sedeño <164188234+Maniraul@users.noreply.github.com>
2025-03-06 10:22:32 +0100

    Renovacion styles EspUsuario

--- EspUsuario/espaciousuario.php
+++ EspUsuario/espaciousuario.php
@@ -43,7 +43,7 @@ if (isset($_SESSION['nombre_usuario'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tomás Suárez - Jugador Profesional</title>
+    <title>Espacio Usuario - GOD</title>
     <link rel="stylesheet" href="styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
